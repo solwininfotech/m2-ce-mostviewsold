8e86b6e832340a155d50cb5245b4501bd9053e2d
Your Name <mmdhudasia@gmail.com>
2020-07-29 16:35:59 +0530

    Initial release

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Most View Sold Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_MostViewSold
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_MostViewSold',
+    __DIR__
+);
